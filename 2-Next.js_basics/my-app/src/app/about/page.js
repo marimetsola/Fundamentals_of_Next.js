273d5a0d6a0c9f8c28c796b781d4bf9dd9bd0028
Mari Metsola <mari.m.metsola@gmail.com>
2025-12-21 01:56:25 +0200

    add task 1.2

---
+++ 2-Next.js_basics/my-app/src/app/about/page.js
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>About this course</h1>
+      <p>
+        This is a fully virtual course that goes through all the important
+        aspects of Next.js.
+      </p>
+    </div>
+  );
+}
