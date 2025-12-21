f4baaddaafd5d487f68849b44b91425647b3959e
Mari Metsola <mari.m.metsola@gmail.com>
2025-12-21 23:41:21 +0200

    add task 1.5

--- 2-Next.js_basics/my-app/src/app/page.tsx
+++ 2-Next.js_basics/my-app/src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import Link from "next/link";
+import styles from "./home/home.module.css";
 
 export default function Home() {
   return (
-    <div className="font-sans">
-      <Link href="/about">Go to About Page</Link>
-      <h1>Mari's fabulous app 👸</h1>
+    <div className={`font-mono ${styles.container}`}>
+      <Link className={styles.link} href="/about">
+        Go to About Page
+      </Link>
+      <h1 className={styles.title}>Mari's fabulous app 👸</h1>
       <p>I have no idea what this app is about. I'm sure it will be great!</p>
     </div>
   );
