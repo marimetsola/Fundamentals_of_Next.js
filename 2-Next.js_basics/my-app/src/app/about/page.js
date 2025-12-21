beb5f304df8977ae0baed72b3e54689bdbb0abb4
Mari Metsola <mari.m.metsola@gmail.com>
2025-12-21 02:23:28 +0200

    add tasks 1.3 & 1.4

--- 2-Next.js_basics/my-app/src/app/about/page.js
+++ 2-Next.js_basics/my-app/src/app/about/page.js
@@ -1,6 +1,10 @@
+"use client";
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div>
+      <Link href="/">Back to Home</Link>
       <h1>About this course</h1>
       <p>
         This is a fully virtual course that goes through all the important
