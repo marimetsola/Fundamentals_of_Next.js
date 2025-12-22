336eeb4fe5859afc465e9f6ece55ea250cbd3013
Mari Metsola <mari.m.metsola@gmail.com>
2025-12-22 13:26:33 +0200

    add task 2.1

--- 2-Next.js_basics/my-app/src/app/page.tsx
+++ 2-Next.js_basics/my-app/src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
+import Title from "./components/Title";
 import styles from "./home/home.module.css";
 
 export default function Home() {
   return (
     <div className={`font-mono ${styles.container}`}>
-      <h1 className={styles.title}>Mari's fabulous app 👸</h1>
+      <Title>Mari's fabulous app 👸</Title>
       <p>I have no idea what this app is about. I'm sure it will be great!</p>
     </div>
   );
