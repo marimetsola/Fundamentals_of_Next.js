0edd15395f6b2a91418f12e5ac3c00026b783c71
Mari Metsola <mari.m.metsola@gmail.com>
2025-12-31 19:02:05 +0200

    add task 2.6

--- 3-Styling_in_Next.js/my-app/src/app/page.tsx
+++ 3-Styling_in_Next.js/my-app/src/app/page.tsx
@@ -4,11 +4,13 @@ import Button from "./components/Button";
 import styles from "./home/home.module.css";
 import Card from "./components/Card";
 import AppLayout from "./components/AppLayout";
+import Alert from "./components/Alert";
 
 export default function Home() {
   return (
     <AppLayout>
       <div className={`font-nova ${styles.container}`}>
+        <Alert />
         <Title>Mari's fabulous app 👸</Title>
         <p>I have no idea what this app is about. I'm sure it will be great!</p>
         <br />
