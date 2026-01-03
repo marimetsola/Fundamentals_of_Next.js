14303c81bc7c7391dff9a30d7bf0ec135aee8791
Mari Metsola <mari.m.metsola@gmail.com>
2026-01-03 02:35:58 +0200

    add task 3.4

--- 4-API_Routes_in_Next.js/my-app/src/app/api/contact/route.js
+++ 4-API_Routes_in_Next.js/my-app/src/app/api/contact/route.js
@@ -2,8 +2,16 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
   const body = await req.json();
+  const { name, message } = body;
 
-  console.log(`Received message "${body.message}" from ${body.name}`);
+  if (!name || !message) {
+    return NextResponse.json(
+      { error: "Name and message are required" },
+      { status: 400 }
+    );
+  }
+
+  console.log(`Received message "${message}" from ${name}`);
 
   return NextResponse.json(
     { success: true, message: "Message received" },
