31b13f8e3f81f9dece5cd5cf1d51336f321feb5d
Mari Metsola <mari.m.metsola@gmail.com>
2026-01-03 02:56:14 +0200

    add task 3.5

--- 4-API_Routes_in_Next.js/my-app/src/app/api/contact/route.js
+++ 4-API_Routes_in_Next.js/my-app/src/app/api/contact/route.js
@@ -4,6 +4,10 @@ export async function POST(req) {
   const body = await req.json();
   const { name, message } = body;
 
+  if (req.headers.get("authorization") !== "Bearer token666") {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   if (!name || !message) {
     return NextResponse.json(
       { error: "Name and message are required" },
@@ -15,6 +19,6 @@ export async function POST(req) {
 
   return NextResponse.json(
     { success: true, message: "Message received" },
-    { status: 200 }
+    { status: 201 }
   );
 }
