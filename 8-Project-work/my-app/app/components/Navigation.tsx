3f403c2b003abb8c6cadd55da880f36899258ffa
Mari Metsola <mari.m.metsola@gmail.com>
2026-02-13 18:59:07 +0200

    make the navbar sticky

--- 8-Project-work/my-app/app/components/Navigation.tsx
+++ 8-Project-work/my-app/app/components/Navigation.tsx
@@ -1,26 +1,23 @@
 import Link from "next/link";
 
 export default function Navigation() {
-    return (
-        <nav className="flex h-[4rem] w-full items-center justify-between px-20 border-b">
-            <div className="text-lg font-semibold">
-                MyApp
-            </div>
-
-            <div className="flex items-center justify-center gap-6">
-                <Link href="/" className="hover:underline">
-                    Home
-                </Link>
-                <Link href="/calculator" className="hover:underline">
-                    Calculator
-                </Link>
-                <Link href="/editor" className="hover:underline">
-                    Editor
-                </Link>
-                <Link href="/quiz" className="hover:underline">
-                    Quiz
-                </Link>
-            </div>
-        </nav>
-    );
+  return (
+    <header className="sticky top-0 z-50 flex w-full items-center justify-between h-16 px-20 border-b bg-background">
+      <div className="text-lg font-semibold">MyApp</div>
+      <nav className="flex items-center justify-center gap-6">
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/calculator" className="hover:underline">
+          Calculator
+        </Link>
+        <Link href="/editor" className="hover:underline">
+          Editor
+        </Link>
+        <Link href="/quiz" className="hover:underline">
+          Quiz
+        </Link>
+      </nav>
+    </header>
+  );
 }
--- 8-Project-work/my-app/app/page.tsx
+++ 8-Project-work/my-app/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-[2000px]">
       <main>
         <h1>Project web app</h1>
-        <p>The project started off with a quick recap on JavaScript and React, followed by
-          overview of Next.js's features. Different ways (CSS modules, global, inline, tailwind) to style Next.js apps were covered.
+        <p>
+          The project started off with a quick recap on JavaScript and React,
+          followed by overview of Next.js&apos;s features. Different ways (CSS
+          modules, global, inline, tailwind) to style Next.js apps were covered.
         </p>
       </main>
     </div>
